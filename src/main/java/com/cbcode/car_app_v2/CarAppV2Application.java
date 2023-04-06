08dada6ef99e54ac7f2ca9cfc67b4eb39fba3a0c
lucleber13 <lucleber13@hotmail.com>
2023-04-07 00:17:14 +0100

    Starting the project

---
+++ src/main/java/com/cbcode/car_app_v2/CarAppV2Application.java
@@ -0,0 +1,13 @@
+package com.cbcode.car_app_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarAppV2Application.class, args);
+    }
+
+}
