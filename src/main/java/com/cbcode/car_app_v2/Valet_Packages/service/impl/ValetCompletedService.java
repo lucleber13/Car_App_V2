77bb2ee8aeb6c23dafdaa86e93855b077e9591c1
lucleber13 <lucleber13@hotmail.com>
2023-04-10 23:19:41 +0100

    documenting methods

--- src/main/java/com/cbcode/car_app_v2/Valet_Packages/service/impl/ValetCompletedService.java
+++ src/main/java/com/cbcode/car_app_v2/Valet_Packages/service/impl/ValetCompletedService.java
@@ -12,10 +12,17 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * This class will be used to add a valet completed to the database and to get all the valets completed.
+ * It will also run a scheduled method that will delete all the valets completed that are older than 61 days.
+ * It will use the ValetCompletedRepository class to interact with the database.
+ * It will use the ModelMapper class to map the ValetCompletedDto to the ValetCompleted entity.
+ * Author: Cleber Balbinote
+ * Date: 10/04/2023
+ */
 @Service
 public class ValetCompletedService {
     private final ValetCompletedRepository valetCompletedRepository;
@@ -25,21 +32,30 @@ public class ValetCompletedService {
         this.valetCompletedRepository = valetCompletedRepository;
         this.modelMapper = modelMapper;
     }
-    // This method will be called from the ValetService class when the valet is finished.
-    // Then, this method will save the object in this database using the ValetCompletedRepository.
+
+    /**
+     * This method will be called from the ValetCompletedController class. It will add a valet completed to the database.
+     * It will use the model mapper to map the valetCompletedDto to the ValetCompleted entity.
+     * @param valetCompletedDto
+     * @return ValetCompletedDto
+     */
     public ValetCompletedDto addValetCompleted(ValetCompletedDto valetCompletedDto) {
         ValetCompleted valetCompleted = modelMapper.map(valetCompletedDto, ValetCompleted.class);
         ValetCompleted savedValetCompleted = valetCompletedRepository.save(valetCompleted);
         return modelMapper.map(savedValetCompleted, ValetCompletedDto.class);
     }
 
+    /**
+     * This method will be called from the ValetCompletedController class. It will return a list of all the valets completed.
+     * @return List<ValetCompletedDto>
+     */
     public List<ValetCompletedDto> getAllValetsCompleted() {
         ValetCompleted valetCompleted = modelMapper.map(valetCompletedRepository.findAll(), ValetCompleted.class);
         if (valetCompleted == null) {
             throw new CarNotFoundException("No valets completed yet.");
         } else {
             List<ValetCompleted> valetCompletedList = valetCompletedRepository.findAll();
-            List<ValetCompletedDto> valetCompletedDtoList = new ArrayList<>();
+            List<ValetCompletedDto> valetCompletedDtoList;
             valetCompletedDtoList = valetCompletedList.stream()
                     .map(allValetsCompleted -> modelMapper.map(allValetsCompleted, ValetCompletedDto.class))
                     .collect(Collectors.toList());
@@ -47,6 +63,10 @@ public class ValetCompletedService {
         }
     }
 
+    /**
+     * This method will run every day at midnight. It will delete all the valets completed that are older than 61 days.
+     * This is done to keep the database clean.
+     */
     @Transactional
     @Modifying
     @Scheduled(cron = "0 0 0 * * *") // This method will run every day at midnight.
